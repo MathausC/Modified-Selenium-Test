2f5a64881821a8b42fa861b26399fd74ed9c95aa
MathausC <mathaus273@gmail.com>
2023-05-31 00:50:15 -0300

    Add stubs for GoogleTest

--- src/test/java/selenium/test/keyword/GoogleTest.java
+++ src/test/java/selenium/test/keyword/GoogleTest.java
@@ -25,22 +25,6 @@ public class GoogleTest {
     List<String> languageList;
     @BeforeEach
     void setUp() {
-        RemoteWebDriver.addLanguagePropertie("src/test/resources/Google/portuguese-language.properties");
-        RemoteWebDriver.addLanguagePropertie("src/test/resources/Google/english-language.properties");
-        RemoteWebDriver.addLanguagePropertie("src/test/resources/Google/italian-language.properties");
-        RemoteWebDriver.addLanguagePropertie("src/test/resources/Google/spanish-language.properties");
-        RemoteWebDriver.addLanguagePropertie("src/test/resources/Google/hebrew-language.properties");
-        RemoteWebDriver.addLanguagePropertie("src/test/resources/Google/russian-language.properties");
-        RemoteWebDriver.addLanguagePropertie("src/test/resources/Google/arabic-language.properties");
-        languageList = new ArrayList<>();
-        languageList.add("pt");
-        languageList.add("es");
-        languageList.add("en");
-        languageList.add("it");
-        languageList.add("ru");
-        //languageList.add("iw");
-        //languageList.add("ar");
-        //WebDriverManager.chromiumdriver().setup();
         WebDriverManager.chromedriver().setup();
         ChromeOptions options = new ChromeOptions();
         webDriver = new ChromeDriver(options);
@@ -52,22 +36,22 @@ public class GoogleTest {
     }
 
     @Test
-    void TestGooglePage() {
-        for(String languange : languageList) {
-            webDriver.get("https://www.google.com/?hl=" + languange);
-            By googleSearchInput = By.keyword("//*[@value=\"${google-search}\"]");
-            By imFeelingLuckInput = By.keyword("//*[@value=\"${im-feeling-luck}\"]");
-            By gmailButton = By.keyword("//*[text()=\"${gmail}\"]");
-            By imagesButton= By.keyword("//*[text()=\"${images}\"]");
-            isElementPresent(googleSearchInput);
-            isElementPresent(imFeelingLuckInput);
-            isElementPresent(gmailButton);
-            isElementPresent(imagesButton);
-        }
+    void TestEnglishGooglePage() {
+        
     }
 
-    private void isElementPresent(By element) {
-        boolean isPresent = webDriver.findElement(element).isEnabled();
-        Assertions.assertTrue(isPresent);
+    @Test
+    void TestPortugueseGooglePage() {
+        
+    }
+
+    @Test
+    void TestSpanishGooglePage() {
+        
+    }
+
+    @Test
+    void TestItalianGooglePage() {
+        
     }
 }
