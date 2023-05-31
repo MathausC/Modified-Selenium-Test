7acdb71669b6f656b010c7e2c0d7db555f2be0e2
MathausC <mathaus273@gmail.com>
2023-05-31 00:50:39 -0300

    Add stubs for GoogleTestWithKeywords

---
+++ src/test/java/selenium/test/keyword/GoogleTestWithKeywords.java
@@ -0,0 +1,42 @@
+package selenium.test.keyword;
+
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.By;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.chromium.ChromiumDriver;
+import org.openqa.selenium.chromium.ChromiumOptions;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.firefox.FirefoxOptions;
+import org.openqa.selenium.remote.RemoteWebDriver;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class GoogleTest {
+
+    WebDriver webDriver;
+    List<String> languageList;
+    @BeforeEach
+    void setUp() {
+        WebDriverManager.chromedriver().setup();
+        ChromeOptions options = new ChromeOptions();
+        webDriver = new ChromeDriver(options);
+    }
+
+    @AfterEach
+    void tearDown() {
+        webDriver.close();
+    }
+
+    @Test
+    void TestGooglePage() {
+
+    }
+}
